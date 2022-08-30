1744a675274fc7600fc98a0e9e0058b8aa8cb67d
James Lee <j.lee1203@hotmail.com>
2022-08-30 13:07:26 -0700

    commit tree 74f29201db0fda2781f0652490cd8d8db7aa3837

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentence) => {
+    const words = sentence.split(" ")
+    const upperCaseWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.substring(1);
+    }) 
+    return upperCaseWords.join(" ");
+  })
 }
+
+// can make whole string to lowercase .toLowerCase()
+// can use split() to turn each string into an array
+// then use map() to iterate through each string
+// substring() 
+// .join()
